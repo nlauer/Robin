e5358b077857979b3a86046e3ae30405a36897ca
Khallil Mangalji <kmx411@gmail.com>
2012-09-14 22:00:33 -0400

    set up backbone

---
+++ app/assets/javascripts/robin.js.coffee
@@ -0,0 +1,9 @@
+window.Robin =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  init: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Robin.init()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robin::Application.config.secret_token = 'b8aca3c4af50ed7cc398e3022b73b30e8175529901f15d34ef6fcb207675e11449644fcb39c0661ae05f0b8804d1d96124b7b4815c68600337489d21e58c2eee'
